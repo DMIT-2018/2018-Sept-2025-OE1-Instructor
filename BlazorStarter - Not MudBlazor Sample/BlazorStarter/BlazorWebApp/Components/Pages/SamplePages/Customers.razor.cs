8bbf8b00c3d8dfd4aac5046caed8ef8414b51242
Tina Caron <tcaron@nait.ca>
2025-10-22 21:52:00 -0600

    Started Customer Search Page

---
+++ BlazorStarter - Not MudBlazor Sample/BlazorStarter/BlazorWebApp/Components/Pages/SamplePages/Customers.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using OLTPSystem.BLL;
+using OLTPSystem.ViewModels;
+
+namespace BlazorWebApp.Components.Pages.SamplePages
+{
+    public partial class Customers
+    {
+        #region Fields
+        private string lastName = string.Empty;
+        private string phoneNumber = string.Empty;
+
+        private string feedbackMessage = string.Empty;
+        // collected error details.
+        private List<string> errorDetails = new();
+        // general error message.
+        private string errorMessage = string.Empty;
+        private List<CustomerSearchView> CustomerList = [];
+        #endregion
+
+        #region Properties
+        [Inject]
+        protected CustomerService CustomerService { get; set; } = default!;
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; } = default!;
+        private bool hasError => !string.IsNullOrEmpty(errorMessage) || errorDetails.Any();
+        #endregion
+
+        #region Methods
+        //When we moved from LINQPAD we can remove the parameters since the
+        //	fields are on this page
+        public void GetCustomers()
+        {
+            //Always start by clearing messages (feedback and errors)
+            //	You start with this so you don't have repeated messages
+            errorDetails.Clear();
+            errorMessage = string.Empty; //I use string.Empty and not "" because it is smaller
+            feedbackMessage = string.Empty;
+
+            //Wrap the call to any service method in a try/catch
+            try
+            {
+                //change the reference to the Page Fields, not referencing any method parameters
+                var results = CustomerService.GetCustomers(lastName, phoneNumber);
+                if (results.IsSuccess)
+                    CustomerList = results.Value ?? [];
+                else
+                    errorDetails = BlazorHelperClass.GetErrorMessages(results.Errors.ToList());
+            }
+            catch (Exception ex)
+            {
+                //capture any unexpected exceptions
+                errorMessage = ex.Message;
+            }
+        }
+
+        public void EditCustomer(int customerID)
+        {
+            NavigationManager.NavigateTo($"/SamplePages/CustomerEdit/{customerID}");
+        }
+        #endregion
+    }
+}

9633cdd27dc3426548f6148d68b429760e74d16f
Tina Caron <tcaron@nait.ca>
2025-11-19 20:02:19 -0700

    Added List to List Example

---
+++ BlazorStarter - Not MudBlazor Sample/BlazorStarter/BlazorWebApp/Components/Pages/SamplePages/SimpleListToList.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using OLTPSystem.BLL;
+using OLTPSystem.ViewModels;
+
+namespace BlazorWebApp.Components.Pages.SamplePages
+{
+    public partial class SimpleListToList
+    {
+        #region fields
+        private List<InvoicePartView> inventory = [];
+        private List<InvoiceLineView> shoppingCart = [];
+        private string errorMessage = string.Empty;
+        private List<string> errorDetails = [];
+        private string feedbackMessage = string.Empty;
+        #endregion
+
+        #region Properties
+        [Inject]
+        public PartService PartService { get; set; } = default!;
+        #endregion
+
+        #region Methods
+        protected override void OnInitialized()
+        {
+            try
+            {
+                var result = PartService.GetParts();
+                if(result.IsSuccess)
+                {
+                    inventory = result.Value ?? [];
+                }
+                else
+                {
+                    errorDetails = BlazorHelperClass.GetErrorMessages(result.Errors.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+        private void AddPartToCart(InvoicePartView part)
+        {
+            if(part != null)
+            {
+                shoppingCart.Add(new InvoiceLineView
+                {
+                    PartID = part.PartID,
+                    PartDescription = part.Description,
+                    Price = part.Price,
+                    Quantity = 1,
+                    Taxable = part.Taxable
+                });
+            }
+        }
+        private void RemoveFromCart(InvoiceLineView invoiceLineView)
+        {
+            shoppingCart.Remove(invoiceLineView);
+        }
+        #endregion
+    }
+}
---
+++ BlazorStarter - Not MudBlazor Sample/BlazorStarter/OLTPSystem/ViewModels/PartView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLTPSystem.ViewModels
+{
+    public class PartView
+	{
+		public int PartID { get; set; }
+		public int PartCategoryID { get; set; }
+		public string CategoryName { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public decimal Price { get; set; }
+		public decimal Cost { get; set; }
+		public int ROL { get; set; }
+		public int QOH { get; set; }
+		public bool Taxable { get; set; }
+		public bool RemoveFromViewFlag { get; set; }
+	}
+}
